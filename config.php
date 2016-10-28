029e711995b53359c344634195735f05b0a63029
Adam Lamers <adam@scanalyticsinc.com>
2016-10-27 23:39:05 -0500

    Add global config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+namespace PHPUsers;
+
+class Config
+{
+    public static $db_name = "my_app";
+    public static $db_user = "my_app";
+    public static $db_pass = "secret";
+    public static $db_host = "localhost";
+    public static $db_port = 3306;
+}
