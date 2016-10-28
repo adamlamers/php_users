76d19b56c59d9dacd6e579f50a53fbf78d40d3de
Adam Lamers <adam@scanalyticsinc.com>
2016-10-27 23:39:30 -0500

    Add index.php - entry point

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+namespace PHPUsers;
+
+require "vendor/autoload.php";
+require "core/app.php";
+
+$app = new Application();
+
+$app->router->add('POST', '/user/create', 'UserController@create');
+$app->router->add('POST', '/user/(\d+)/delete', 'UserController@remove');
+$app->router->add('POST', '/user/(\d+)/update', 'UserController@update');
+$app->router->add('GET', '/user/(\d+)/get', 'UserController@get');
+
+$app->run();
